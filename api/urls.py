45863e6d85fc69ac292eae8bcacb90a741c90801
Desire1990 <desnduwim26@gmail.com>
2022-03-15 15:59:07 +0200

    base

---
+++ api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include, re_path
+from rest_framework import routers
+from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from dj_rest_auth.views import LoginView, LogoutView, PasswordChangeView, PasswordResetConfirmView, PasswordResetView, UserDetailsView
+
+
+router = routers.DefaultRouter()
+router.register("user",UserViewset)
+router.register("student",StudentViewset)
+
+app_name='api'
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path('login/', TokenPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+    path('password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
+]
